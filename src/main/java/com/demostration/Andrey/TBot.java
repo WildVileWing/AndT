adec4d3f0f6f39808ba833a58ea550202cc8bb71
Andrey Tikholaz <wildvilewingg@gmail.com>
2021-07-07 13:06:35 +0300

    Практическое задание №1. Реализована страница Hello, world

---
+++ src/main/java/com/demostration/Andrey/TBot.java
@@ -0,0 +1,13 @@
+package com.demostration.Andrey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TBot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TBot.class, args);
+	}
+
+}
---
+++ src/test/java/com/demostration/Andrey/TBotTests.java
@@ -0,0 +1,13 @@
+package com.demostration.Andrey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TBotTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
